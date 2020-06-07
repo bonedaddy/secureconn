398e2174ebfa21857c7ed4cb4c928f86aa4efa84
James Mills <prologic@shortcircuit.net.au>
2020-06-07 15:45:33 +1000

    Refactor code and format properly with go fmt

--- client.go
+++ client.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-    "bufio"
-    "errors"
-    "fmt"
-    "net"
-    "os"
+	"bufio"
+	"errors"
+	"fmt"
+	"net"
+	"os"
 )
 
 type Client struct {
-    port int
+	port int
 }
 
 func (c *Client) Connect() error {
-    address := fmt.Sprintf("127.0.0.1:%d", c.port)
-    serverAddress, _ := net.ResolveTCPAddr("tcp", address)
+	address := fmt.Sprintf("127.0.0.1:%d", c.port)
+	serverAddress, _ := net.ResolveTCPAddr("tcp", address)
 
-    conn, err := net.DialTCP("tcp", nil, serverAddress)
-    if err != nil {
-        return errors.New("Problem connecting to server, is it running?\n")
-    }
-    defer conn.Close()
+	conn, err := net.DialTCP("tcp", nil, serverAddress)
+	if err != nil {
+		return errors.New("Problem connecting to server, is it running?\n")
+	}
+	defer conn.Close()
 
-    fmt.Printf("Connection on %s\n", address)
+	fmt.Printf("Connection on %s\n", address)
 
-    sharedKey := Handshake(conn)
-    secureConnection := SecureConnection{conn: conn, sharedKey: sharedKey}
+	sharedKey := Handshake(conn)
+	secureConnection := SecureConnection{conn: conn, sharedKey: sharedKey}
 
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    for {
-        fmt.Print("> ")
-        // Read up to the newline character
-        msg, _ := reader.ReadBytes(0xA)
-        // Kill the newline char
-        msg = msg[:len(msg)-1]
+	for {
+		fmt.Print("> ")
+		// Read up to the newline character
+		msg, _ := reader.ReadBytes(0xA)
+		// Kill the newline char
+		msg = msg[:len(msg)-1]
 
-        _, err := secureConnection.Write(msg)
+		_, err := secureConnection.Write(msg)
 
-        response := make([]byte, 1024)
+		response := make([]byte, 1024)
 
-        _, err = secureConnection.Read(response)
-        if err != nil {
-            fmt.Print("Connection to the server was closed.\n")
-            break
-        }
+		_, err = secureConnection.Read(response)
+		if err != nil {
+			fmt.Print("Connection to the server was closed.\n")
+			break
+		}
 
-        fmt.Printf("%s\n", response)
-    }
+		fmt.Printf("%s\n", response)
+	}
 
-    return nil
+	return nil
 }
--- server.go
+++ server.go
@@ -1,50 +1,49 @@
 package main
 
-import(
-    "net"
-    "fmt"
-    "os"
+import (
+	"fmt"
+	"net"
+	"os"
 )
 
 type Server struct {
-  port int
+	port int
 }
 
 func (s *Server) Run() {
-    address := fmt.Sprintf("127.0.0.1:%d", s.port)
-    networkAddress, _ := net.ResolveTCPAddr("tcp", address)
+	address := fmt.Sprintf("127.0.0.1:%d", s.port)
+	networkAddress, _ := net.ResolveTCPAddr("tcp", address)
 
-    listener, err := net.ListenTCP("tcp", networkAddress)
-    if err != nil {
-        fmt.Print(err)
-        os.Exit(2)
-    }
+	listener, err := net.ListenTCP("tcp", networkAddress)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(2)
+	}
 
-    for {
-        conn, err := listener.AcceptTCP()
+	for {
+		conn, err := listener.AcceptTCP()
 
-        if err != nil {
-            fmt.Print(err)
-        }
+		if err != nil {
+			fmt.Print(err)
+		}
 
-
-        go handleConnection(conn)
-    }
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn *net.TCPConn) {
-    defer conn.Close()
-    sharedKey := Handshake(conn)
-    secureConnection := SecureConnection{conn: conn, sharedKey: sharedKey}
+	defer conn.Close()
+	sharedKey := Handshake(conn)
+	secureConnection := SecureConnection{conn: conn, sharedKey: sharedKey}
 
-    for {
-        msg := make([]byte, 1024)
-        _, err := secureConnection.Read(msg)
+	for {
+		msg := make([]byte, 1024)
+		_, err := secureConnection.Read(msg)
 
-        if err != nil {
-            break
-        }
+		if err != nil {
+			break
+		}
 
-        secureConnection.Write(msg)
-    }
+		secureConnection.Write(msg)
+	}
 }
